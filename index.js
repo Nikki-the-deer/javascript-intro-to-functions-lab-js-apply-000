550554d305b6aa47329bf244c457817229492e1f
Nicholas Eubanks <nick-2013@live.com>
2019-01-18 18:44:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,17 +14,6 @@ function logWhisper(string){
   console.log(whisper(string));
 }
 
-var uppercase = "HELLO!";
-uppercase.toUpperCase() === uppercase; //true
-
-var lowercase = 'hello!';
-lowercase.toLowerCase() === lowercase; //true
-
-var mixedCase = 'Hi there!';
-mixedCase.toLowerCase() === mixedCase; //false
-
-mixedCase.toUpperCase() === mixedCase; //false
-
 function sayHiToGrandma(string){
   if (string === uppercase){
   return "YES INDEED!";
