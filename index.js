6c21c62924165157c7e5312deb48047376e92f5c
Nicholas Eubanks <nick-2013@live.com>
2019-01-18 19:02:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   else if(string === string.toLowerCase()){
     return "I can't hear you!";
   }
-  else if(string === "I love you, Grandma"){
+  if(string === "I love you, Grandma"){
     return "I love you, too";
   }
 }
